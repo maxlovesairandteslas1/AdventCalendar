87715ccf247423066130bbd677d1eb4055cd812c
Max <77656052+maxlovesairandteslas1@users.noreply.github.com>
2022-03-09 22:37:17 -0800

    Update question-1.py

--- question-1.py
+++ question-1.py
@@ -1,1 +1,18 @@
-print("hi")
+# Advent Calendar
+# Question no.1
+# By Max Yang
+# Github: Maxlovesairandteslas1
+
+counter = 0
+increased = 0
+for i in range (3000):
+    x = input()
+    x = x.strip(" ")
+    y = x[counter]
+    counter = counter + 1
+    z = x[counter]
+    if z > y:
+        increased = increased + 1
+
+
+print(increased)
